7f12cc9147e9d51da0fe6b8c2e4412e1d5933356
Akashnarayan <asp26121992@gmail.com>
2016-12-16 17:00:54 +0530

    Patatap clone built using paper.js and howler.js

---
+++ key.js
@@ -0,0 +1,158 @@
+var keyData = {
+    a:{
+        sound:new Howl({
+            src:['A/bubbles.mp3']
+        }),
+        color:'#7CEECE'
+    },
+    b:{
+        sound:new Howl({
+            src:['A/clay.mp3']
+        }),
+        color:'#92F22A'
+    },
+    c:{
+        sound:new Howl({
+            src:['A/confetti.mp3']
+        }),
+        color:'#FF7416'
+    },
+    d:{
+        sound:new Howl({
+            src:['A/corona.mp3']
+        }),
+        color:'#FD5B03'
+    },
+    e:{
+        sound:new Howl({
+            src:['A/dotted-spiral.mp3']
+        }),
+        color:'#3D8EB9'
+    },
+    f:{
+        sound:new Howl({
+            src:['A/flash-1.mp3']
+        }),
+        color:'#EB6361'
+    },
+    g:{
+        sound:new Howl({
+            src:['A/flash-2.mp3']
+        }),
+        color:'#FEC606'
+    },
+    h:{
+        sound:new Howl({
+            src:['A/flash-3.mp3']
+        }),
+        color:'#EBBD63'
+    },
+    i:{
+        sound:new Howl({
+            src:['A/glimmer.mp3']
+        }),
+        color:'#1DABB8'
+    },
+    j:{
+        sound:new Howl({
+            src:['A/moon.mp3']
+        }),
+        color:'#FFFFF7'
+    },
+    k:{
+        sound:new Howl({
+            src:['A/pinwheel.mp3']
+        }),
+        color:'#FACA9B'
+    },
+    l:{
+        sound:new Howl({
+            src:['A/piston-1.mp3']
+        }),
+        color:'#AAB69B'
+    },
+    m:{
+        sound:new Howl({
+            src:['A/piston-2.mp3']
+        }),
+        color:'#2C82C9'
+    },
+    n:{
+        sound:new Howl({
+            src:['A/piston-3.mp3']
+        }),
+        color:'#D98B3A'
+    },
+    o:{
+        sound:new Howl({
+            src:['A/prism-1.mp3']
+        }),
+        color:'#BB3658'
+    },
+    p:{
+        sound:new Howl({
+            src:['A/prism-2.mp3']
+        }),
+        color:'#EFE0B1'
+    },
+    q:{
+        sound:new Howl({
+            src:['A/prism-3.mp3']
+        }),
+        color:'#EAF2BB'
+    },
+    r:{
+        sound:new Howl({
+            src:['A/splits.mp3']
+        }),
+        color:'#D8E2EC'
+    },
+    s:{
+        sound:new Howl({
+            src:['A/squiggle.mp3']
+        }),
+        color:'#E1FAFC'
+    },
+    t:{
+        sound:new Howl({
+            src:['A/strike.mp3']
+        }),
+        color:'#B17E22'
+    },
+    u:{
+        sound:new Howl({
+            src:['A/suspension.mp3']
+        }),
+        color:'#95A5A6'
+    },
+    v:{
+        sound:new Howl({
+            src:['A/timer.mp3']
+        }),
+        color:'#9EA97F'
+    },
+    w:{
+        sound:new Howl({
+            src:['A/ufo.mp3']
+        }),
+        color:'#BBB7A4'
+    },
+    x:{
+        sound:new Howl({
+            src:['A/veil.mp3']
+        }),
+        color:'#85E2FF'
+    },
+    y:{
+        sound:new Howl({
+            src:['A/wipe.mp3']
+        }),
+        color:'#B0A297'
+    },
+    z:{
+        sound:new Howl({
+            src:['A/zig-zag.mp3']
+        }),
+        color:'#EEC3D6'
+    }
+};
